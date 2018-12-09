ae708f78e3dfa42cfe5e9f0abf0f846d80e30514
Madaoo <kielkowskimarcin@prokonto.pl>
2018-12-09 13:02:54 +0100

    add false test cases

---
+++ test/cbo_mp3.py
@@ -0,0 +1,16 @@
+import unittest
+
+class CBOMp3(unittest.TestCase):
+
+    def test_0_giet_mp3_files(self):
+        self.assertEquals(1, 2)
+
+    def test_1_check_file_to_convert(self):
+        self.assertEquals(1, 2)
+
+    def test_2_convert_files(self):
+        self.assertEquals(1, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
