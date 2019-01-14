9b4d875b4e4992654f4dc6f076e7a1aba10392c2
Madaoo <kielkowskimarcin@prokonto.pl>
2019-01-14 18:41:06 +0100

    Fix style errors

--- core/cbo_mp3.py
+++ core/cbo_mp3.py
@@ -3,27 +3,24 @@ import ffmpy
 import os
 from mutagen.mp3 import MP3
 
+
 class CBOMp3:
 
-    def get_mp3_files(self, dir_path):
+    @staticmethod
+    def get_mp3_files(dir_path):
         """
         Scan selected directory for search all mp3 files
         dir_path: path for selected directory
         :return: list of mp3 files
         """
-        mp3_files = None
-        try:
-            mp3_files = glob.glob("{}*.mp3".format(dir_path))
-        except:
-            print("No mp3 files found!")
-        return mp3_files
+        return glob.glob("{}*.mp3".format(dir_path))
 
-    def check_file_to_convert(self, mp3_files):
+    @staticmethod
+    def check_file_to_convert(mp3_files):
         """
         check bitrate of mp3 file and remove
         :return:
         """
-        print(mp3_files)
         for mp3_file in mp3_files:
             audio = MP3(mp3_file)
             if audio.info.bitrate > 128000:
@@ -40,13 +37,14 @@ class CBOMp3:
         """
         for mp3_file in mp3_files:
             dst_mp3 = self.create_dst_location(mp3_file)
-            print(dst_mp3)
             ff = ffmpy.FFmpeg(
                 inputs={mp3_file: None},
                 outputs={dst_mp3: '-acodec libmp3lame -ab 128k'}
             )
-            print(ff.cmd)
-            ff.run()
+            try:
+                ff.run()
+            except ffmpy.FFRuntimeError:
+                print('File {} already exists.'.format(dst_mp3))
 
     def create_dst_location(self, mp3_file):
         """
@@ -55,15 +53,15 @@ class CBOMp3:
         :return:
         """
         mp3_file = mp3_file.split("\\")
-        song = mp3_file[len(mp3_file)-1]
-        # hardcodet tmp location
-        mp3_file[len(mp3_file)-1] = "tmp"
+        song = mp3_file[len(mp3_file) - 1]
+        mp3_file[len(mp3_file) - 1] = "tmp"
         # join
         dst_file = '\\'.join([str(x) for x in mp3_file])
         self.create_tmp_dir(dst_file)
         return dst_file + "\\{}".format(song)
 
-    def create_tmp_dir(self, dir_name):
+    @staticmethod
+    def create_tmp_dir(dir_name):
         """
 
         :param dir_name:
@@ -72,3 +70,9 @@ class CBOMp3:
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
 
+
+if __name__ == "__main__":
+    cbo = CBOMp3()
+    mp3_files = cbo.get_mp3_files('F:\\Robocik\\')
+    mp3_files = cbo.check_file_to_convert(mp3_files)
+    cbo.convert_files(mp3_files)
